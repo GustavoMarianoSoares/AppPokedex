c2a2aa431752da611196379adbedb978403ca53b
Mariano <gustavomarianosoares@gmail.com>
2022-04-22 22:51:11 -0300

    Api mostrano no Terminal

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from '../services/api'
 
 export default function Routes() {
+
+    useEffect(() =>{
+        api.get('1').then((response) => {
+            console.log(response);
+        })
+    }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>TESTE</Text>
       <StatusBar style="auto" />
+      <Text>TESTE</Text>
+
     </View>
   );
 }
